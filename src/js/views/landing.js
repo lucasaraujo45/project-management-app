0305df12f1c73c3773cb0c89a87d0f77b5bcf9d0
Eduard Chiticari <eduard.kitikari@gmail.com>
2020-02-06 06:32:07 +0000

    created login view

--- src/js/views/landing.js
+++ src/js/views/landing.js
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 export const Landing = () => (
 	<div className="text-center mt-5">
 		<h1>THIS IS A LANDING PAGE</h1>
-		<Link to="/login">Login</Link>
+		<button>
+			<Link to="/login">Login</Link>
+		</button>
 	</div>
 );
--- src/js/views/login.js
+++ src/js/views/login.js
@@ -1,9 +1,60 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../../styles/login.scss";
 
 export const Login = () => (
-	<div className="text-center mt-5">
-		<h1>THIS one is A Login Page</h1>
-		<Link to="/dashboard">Dashbord</Link>
-	</div>
+	<form className="w-50 mx-auto border border-warning p-4">
+		<div className="d-flex mx-auto my-2">
+			<img
+				src="https://cdn0.iconfinder.com/data/icons/professional-avatar-5/48/Gamer_male_avatar_men_character_professions-512.png"
+				alt="Avatar"
+				className="mx-auto bg-primary rounded-circle"
+				width="25%"
+			/>
+		</div>
+
+		<div className="container">
+			<div className="container">
+				<div>
+					<label htmlFor="uname">
+						<b>Username</b>
+					</label>
+				</div>
+				<div>
+					<input type="text" placeholder="Enter Username" className="form-control" required />
+				</div>
+			</div>
+
+			<div className="container mb-3">
+				<div>
+					<label htmlFor="psw">
+						<b>Password</b>
+					</label>
+				</div>
+				<input type="password" placeholder="Enter Password" className="form-control" required />
+			</div>
+			<div className="container">
+				<Link to="/dashboard">
+					<button type="submit">Login</button>
+				</Link>
+				<label className="float-right">
+					<input type="checkbox" name="remember" /> Remember me
+				</label>
+			</div>
+		</div>
+		<hr />
+
+		<div className="container">
+			<div className="container">
+				<Link to="/">
+					<button type="button" className="cancelbtn">
+						Cancel
+					</button>
+				</Link>
+				<span className="mx-2">
+					Forgot <a href="#">password?</a>
+				</span>
+			</div>
+		</div>
+	</form>
 );
