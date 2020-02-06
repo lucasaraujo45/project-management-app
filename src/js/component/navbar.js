94645f631a9891f5d58a37792ba9c9a38268232e
Eduard Chiticari <eduard.kitikari@gmail.com>
2020-02-06 16:10:01 +0000

    changed navbar, changed website title

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -5,7 +5,7 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1">Project Managment tool</span>
 			</Link>
 			<div className="ml-auto">
 				<Link to="/login">
