8558fe652af4a98de9c85aa5f8a208c219372da6
Eduard Chiticari <eduard.kitikari@gmail.com>
2020-02-07 21:24:17 +0000

    Added Dropdown, React-Bootstrap

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -25,48 +25,6 @@ export const Navbar = () => {
 						<Dropdown.Item href="/">Home</Dropdown.Item>
 					</Dropdown.Menu>
 				</Dropdown>
-
-				{/* <button type="button" className="btn btn-primary mr-1" data-toggle="modal" data-target="#exampleModal">
-					Launch demo modal
-				</button>
-
-				<div
-					className="modal fade"
-					id="exampleModal"
-					tabIndex="-1"
-					role="dialog"
-					aria-labelledby="exampleModalLabel"
-					aria-hidden="true">
-					<div className="modal-dialog" role="document">
-						<div className="modal-content">
-							<div className="modal-header">
-								<h5 className="modal-title" id="exampleModalLabel">
-									Already a User?
-								</h5>
-								<button type="button" className="close" data-dismiss="modal" aria-label="Close">
-									<span aria-hidden="true">&times;</span>
-								</button>
-							</div>
-							<div className="modal-body">
-								<Link to="/login">
-									<button type="button" className="btn btn-primary">
-										Login
-									</button>
-								</Link>
-								<Link to="/signup">
-									<button type="button" className="btn btn-primary">
-										Sing Up
-									</button>
-								</Link>
-							</div>
-							<div className="modal-footer">
-								<button type="button" className="btn btn-secondary" data-dismiss="modal">
-									Close
-								</button>
-							</div>
-						</div>
-					</div>
-				</div> */}
 			</div>
 		</nav>
 	);
