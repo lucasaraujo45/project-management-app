5cee0deee561b371264c4fcc612d6fdba79b3d5f
Eduard Chiticari <eduard.kitikari@gmail.com>
2020-02-08 01:20:15 +0000

    Added Sidebar, restyled arrow Button

--- src/js/views/dashboard.js
+++ src/js/views/dashboard.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { Tasks } from "../component/dashboard/tasks/tasks";
+import { SideBar } from "../component/dashboard/sidebar/sidebar";
 
 export const Dashboard = () => (
 	<div className="mt-5">
+		<SideBar />
 		<Tasks />
 	</div>
 );
