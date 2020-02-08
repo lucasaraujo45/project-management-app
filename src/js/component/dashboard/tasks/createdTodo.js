7d11b7ade4d9f4ee3c4ee199afd20ea906eb4685
Lucas Dearaujo <lucasaraujo45@outlook.com>
2020-02-08 00:10:06 +0000

    added modal for add task

--- src/js/component/dashboard/tasks/createdTodo.js
+++ src/js/component/dashboard/tasks/createdTodo.js
@@ -1,5 +1,7 @@
 import React, { useContext } from "react";
 import { Context } from "../../../store/appContext";
+import Modal from "@material-ui/core/Modal";
+import Button from "@material-ui/core/Button";
 
 export const CreateTodo = () => {
 	const { store, actions } = useContext(Context);
@@ -9,13 +11,43 @@ export const CreateTodo = () => {
 		e.preventDefault();
 		actions.addTodo(newTodo.current.value);
 		newTodo.current.value = "";
+		toggleModal();
 	};
 
+	//////////////material ui modal open and close////////////
+	const [open, setOpen] = React.useState(false);
+
+	const toggleModal = () => {
+		setOpen(!open);
+	};
+	//////////////material ui modal open and close////////////
 	return (
 		<div>
-			<form onSubmit={createNewTodo} className="mb-5">
-				<input className="form-control" type="text" ref={newTodo} />
-			</form>
+			<div className="taskButton">
+				{" "}
+				<Button type="button" variant="contained" color="primary" onClick={toggleModal}>
+					Add Task
+				</Button>
+			</div>
+			<Modal
+				aria-labelledby="simple-modal-title"
+				aria-describedby="simple-modal-description"
+				open={open}
+				onClose={toggleModal}
+				className="mt-5">
+				<div className="taskModal">
+					<h1>Add Task</h1>
+					<form onSubmit={createNewTodo} className="mb-5">
+						<input className="form-control" type="text" ref={newTodo} />
+					</form>
+					<Button type="button" variant="contained" color="primary" className="mr-3" onClick={createNewTodo}>
+						Add Task
+					</Button>
+					<Button type="button" variant="contained" onClick={toggleModal}>
+						Close
+					</Button>
+				</div>
+			</Modal>
 		</div>
 	);
 };
