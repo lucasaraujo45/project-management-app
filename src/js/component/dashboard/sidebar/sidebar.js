69cd5deddc7fdf326f2b7977d2b077d9711536f3
Lucas Dearaujo <lucasaraujo45@outlook.com>
2020-02-25 15:07:51 +0000

    made dashboard responsive

--- src/js/component/dashboard/sidebar/sidebar.js
+++ src/js/component/dashboard/sidebar/sidebar.js
@@ -202,10 +202,10 @@ export const SideBar = () => {
 			<main className={classes.content}>
 				<div className={classes.toolbar}>
 					<Grid container spacing={3}>
-						<Grid item xs={7}>
+						<Grid item md={12} lg={7}>
 							<Tasks />
 						</Grid>
-						<Grid item xs={5}>
+						<Grid item md={12} lg={5}>
 							<CompletedTasks />
 						</Grid>
 					</Grid>
