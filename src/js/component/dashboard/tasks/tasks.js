2b2628d15dc8b7baa52a7bb81d524ff94a209654
Lucas Dearaujo <lucasaraujo45@outlook.com>
2020-03-28 00:38:06 +0000

    adjusted table

--- src/js/component/dashboard/tasks/tasks.js
+++ src/js/component/dashboard/tasks/tasks.js
@@ -50,12 +50,12 @@ export const Tasks = props => {
 								<TableCell />
 								<TableCell />
 								<TableCell />
-								<TableCell />
 								<TableCell align="right">
 									<h4>
 										<span>{pluralize(store.list.length)}</span>
 									</h4>
 								</TableCell>
+								<TableCell />
 
 								<TableCell align="right">
 									<CreateTodo />
